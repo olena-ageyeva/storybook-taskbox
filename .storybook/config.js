9851eaea2575d8bc00514730a1263e6107fbbb67
olena-ageyeva <eageeva!gmail.com>
2019-11-10 18:02:25 -0500

    css and babel added

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,14 @@
-import { configure } from '@storybook/react';
+// .storybook/config.js
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+import { configure } from "@storybook/react";
+import requireContext from "require-context.macro";
+
+import "../src/index.css";
+
+const req = requireContext("../src/components", true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
